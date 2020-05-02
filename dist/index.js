edd6a36b2b2ef46bf1621c7bb99b11f58011ac58
anderstofte <ande587e@stud.kea.dk>
2020-05-02 15:45:44 +0200

    remove web.config before deleting .exe

--- dist/index.js
+++ dist/index.js
@@ -1388,6 +1388,19 @@ const core = __webpack_require__(694);
 const github = __webpack_require__(30);
 const ftp = __webpack_require__(602)
 
+// try {
+//   // `who-to-greet` input defined in action metadata file
+//   const nameToGreet = core.getInput('who-to-greet');
+//   console.log(`Hello ${nameToGreet}!`);
+//   const time = (new Date()).toTimeString();
+//   core.setOutput("time", time);
+//   // Get the JSON webhook payload for the event that triggered the workflow
+//   const payload = JSON.stringify(github.context.payload, undefined, 2)
+//   console.log(`The event payload: ${payload}`);
+// } catch (error) {
+//   core.setFailed(error.message);
+// }
+
 
 try {
     example()
@@ -1406,14 +1419,14 @@ async function example() {
             password: core.getInput('password'),
             secure: true
         })
-        console.log(await client.list())
         await client.ensureDir(core.getInput('remote-dir'))
-        await client.list(core.getInput('remote-dir'))
-        await client.clearWorkingDir()
-        await client.uploadFromDir(core.getInput('local-dir'))
+         // Always delete web.config first as it's preventing .exe to be deleted.
+         await client.remove('web.config')
+         await client.clearWorkingDir()
+         await client.uploadFromDir(core.getInput('local-dir'))
     }
     catch(err) {
-        console.log(err)
+        core.setFailed(error.message);
     }
     client.close()
 }
